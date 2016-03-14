7a08ecd0e46e287829d583add2683659d99e5de1
user <szalewskipawel@gmail.com>
2016-03-14 14:19:18 +0100

    webpack images config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 var webpack = require("webpack");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
 
 module.exports = {
     entry: __dirname+"/src/js/entry.js",
     output: {
         path: __dirname+'/public',
         filename: "bundle.js"
+        //publicPath: __dirname+'/public'
     },
     module: {
         loaders: [
@@ -13,7 +15,16 @@ module.exports = {
               test: /\.scss$/,
               loader: ExtractTextPlugin.extract(
                 "style",
-                "css!sass")
+                "css!sass?includePaths[]="+ path.resolve(__dirname, "./node_modules/compass-mixins/lib")
+              )
+            },/*
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: "file-loader?name=../images/img-[hash:6].[ext]"
+            }*/
+            { 
+                test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
+                loader: 'url-loader' 
             }
         ]
     },
